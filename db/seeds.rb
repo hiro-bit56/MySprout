072b4d00c24213af0a73dab3bca257707cebbbf2
hiro-bit56 <ghq458dfmc16@gmail.com>
2026-07-10 06:24:58 +0900

    シードデータ作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,18 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+puts "テストデータを作成します"
+
+# 日付オブジェクトの配列を作成
+start_date = Date.new(2026, 5, 1)
+end_date   = Date.new(2026, 7, 10)
+date_array = (start_date..end_date).to_a
+
+mood_list = ["best", "good", "usually", "bad", "worst"]
+
+date_array.each do |date|
+  record = MoodRecord.new
+  record.user_id = 1
+  record.mood_level = mood_list.sample
+  record.record_on = date
+  record.save!
+end
+  
+puts "テストデータを #{MoodRecord.count} 件作成しました"
